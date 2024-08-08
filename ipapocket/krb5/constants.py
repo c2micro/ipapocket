0a5fae52f7c38417b62d967deebfaade31e9531d
38dh91 <>
2024-08-08 13:06:24 +0300

    update enc types and cksum

--- ipapocket/krb5/constants.py
+++ ipapocket/krb5/constants.py
@@ -178,18 +178,23 @@ class ErrorCodes(enum.Enum):
 # https://web.mit.edu/kerberos/krb5-devel/doc/admin/enctypes.html
 # https://www.freeipa.org/page/Releases/4.8.0#highlights-in-4-8-0 (using only AES types since 4.8.0)
 # https://www.freeipa.org/page/Releases/4.8.2#enhancements (default AES 256/384 since 4.8.2)
+# Only types, that are used in FreeIPA (without deprecated)
 class EncryptionTypes(enum.Enum):
-    DES_CBC_CRC = 1 # DES cbc mode with CRC-32 (deprecated)
-    DES_CBC_MD4 = 2 # DES cbc mode with RSA-MD4 (deprecated)
-    DES_CBC_MD5 = 3 # DES cbc mode with RSA-MD5 (deprecated)
-    DES_CBC_RAW = 4 # DES cbc mode raw (deprecated)
-    DES3_CBC_RAW = 6 # Triple DES cbc mode raw (deprecated)
     DES3_CBC_SHA1 = 16 # Triple DES cbc mode with HMAC/sha1
-    AES128_CTS_HMAC_SHA1_96 = 17 # AES-128 CTS mode with 96-bit SHA-1 HMAC
-    AES256_CTS_HMAC_SHA1_96 = 18 # AES-256 CTS mode with 96-bit SHA-1 HMAC
-    AES128_CTS_HMAC_SHA256_128 = 19 # AES-128 CTS mode with 128-bit SHA-256 HMAC
-    AES256_CTS_HMAC_SHA384_192 = 20 # AES-256 CTS mode with 192-bit SHA-384 HMAC
-    ARCFOUR_HMAC = 23 # ArcFour with HMAC/md5
-    ARCFOUR_HMAC_EXP = 24 # Exportable ArcFour with HMAC/md5 (deprecated)
-    CAMELLIA128_CTS_CMAC = 25 # Camellia-128 CTS mode with CMAC
-    CAMELLIA256_CTS_CMAC = 26 # Camellia-256 CTS mode with CMAC
+    AES128_CTS_HMAC_SHA1_96 = 17 # AES-128 CTS mode with 96-bit SHA-1 HMAC (https://www.rfc-editor.org/rfc/rfc3962)
+    AES256_CTS_HMAC_SHA1_96 = 18 # AES-256 CTS mode with 96-bit SHA-1 HMAC (https://www.rfc-editor.org/rfc/rfc3962)
+    AES128_CTS_HMAC_SHA256_128 = 19 # AES-128 CTS mode with 128-bit SHA-256 HMAC (https://www.rfc-editor.org/rfc/rfc8009)
+    AES256_CTS_HMAC_SHA384_192 = 20 # AES-256 CTS mode with 192-bit SHA-384 HMAC (https://www.rfc-editor.org/rfc/rfc8009)
+    ARCFOUR_HMAC = 23 # ArcFour with HMAC/md5 (https://www.rfc-editor.org/rfc/rfc4757)
+    CAMELLIA128_CTS_CMAC = 25 # Camellia-128 CTS mode with CMAC (https://www.rfc-editor.org/rfc/rfc6803.html)
+    CAMELLIA256_CTS_CMAC = 26 # Camellia-256 CTS mode with CMAC (https://www.rfc-editor.org/rfc/rfc6803.html)
+
+# https://github.com/krb5/krb5/blob/master/src/include/krb5/krb5.hin#L442
+# Only types, that are used in FreeIPA (without unkeyed/deprecated)
+class ChecksumTypes(enum.Enum):
+    HMAC_SHA1_96_AES128 = 15 # https://www.rfc-editor.org/rfc/rfc3962
+    HMAC_SHA1_96_AES256 = 16 # https://www.rfc-editor.org/rfc/rfc3962
+    CMAC_CAMELLIA128 = 17 # https://www.rfc-editor.org/rfc/rfc6803.html
+    CMAC_CAMELLIA256 = 18 # https://www.rfc-editor.org/rfc/rfc6803.html
+    HMAC_SHA256_128_AES128 = 19 # https://www.rfc-editor.org/rfc/rfc8009
+    HMAC_SHA384_192_AES256 = 20 # https://www.rfc-editor.org/rfc/rfc8009

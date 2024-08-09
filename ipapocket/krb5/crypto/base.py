8b19ad091cbb8f202e59849e1225998288d10992
38dh91 <>
2024-08-09 15:57:11 +0300

    add support for all aes rfc in krb

--- ipapocket/krb5/crypto/base.py
+++ ipapocket/krb5/crypto/base.py
@@ -14,16 +14,17 @@ from ipapocket.krb5.crypto.utils import (
 )
 from Cryptodome.Hash import HMAC, SHA1, SHA256, SHA384
 from Cryptodome.Protocol.KDF import PBKDF2
+from ipapocket.krb5.crypto.sp800 import SP800_108_Counter
 import struct
 from ipapocket.krb5.constants import EncryptionTypes
 
 
 class Key(object):
-    def __init__(self, etype: EncryptionTypes, data):
-        e = _get_etype_profile(etype)
+    def __init__(self, enctype: EncryptionTypes, data):
+        e = _get_etype_profile(enctype)
         if len(data) != e.keysize and len(data) != e.macsize:
             raise InvalidKeyLength(len(data))
-        self.enctype = etype
+        self.enctype = enctype
         self.contents = data
 
 
@@ -40,7 +41,7 @@ class _EtypeBaseProfile(object):
     def random_to_key(self, seed):
         if len(seed) != self.seedsize:
             raise InvalidSeedSize(seed, self.seedsize)
-        return Key(self.etype, seed)
+        return Key(self.enctype, seed)
 
 
 class _EtypeRfc3961Profile(_EtypeBaseProfile):
@@ -103,7 +104,7 @@ class _EtypeRfc3961Profile(_EtypeBaseProfile):
         return cls.basic_encrypt(kp, truncated)
 
 
-class _AesXXXSha1(_EtypeRfc3961Profile):
+class _EtypeRfc3962(_EtypeRfc3961Profile):
     blocksize = 16
     padsize = 1
     macsize = 12
@@ -133,34 +134,142 @@ class _AesXXXSha1(_EtypeRfc3961Profile):
         return basic_decrypt_all_aes(cls, key, ciphertext, iv)
 
 
-class _AES128_SHA1(_AesXXXSha1):
-    etype = EncryptionTypes.AES128_CTS_HMAC_SHA1_96
+class _EtypeRfc8009(_EtypeRfc3961Profile):
+    # Base class for aes128-cts-hmac-sha256-128 and aes256-cts-hmac-sha384-192.
+    blocksize = 128 // 8  # Cipher block size
+    seedsize = None  # PRF output size
+    keysize = None  # Encryption key size
+    macsize = None  # Integrity key size
+    hashmod = None  # Hash function module
+    enctype_name = None  # Encryption type name as byte string
+
+    @classmethod
+    def random_to_key(self, seed):
+        return Key(self.enctype, seed)
+
+    @classmethod
+    def basic_encrypt(self, key, plaintext, iv):
+        return basic_encrypt_all_aes(self, key, plaintext, iv)
+
+    @classmethod
+    def basic_decrypt(self, key, ciphertext, iv):
+        return basic_decrypt_all_aes(self, key, ciphertext, iv)
+
+    @classmethod
+    def kdf_hmac_sha2(self, key, label, k, context=b""):
+        hmac_sha2 = lambda p, s: HMAC.new(p, s, self.hashmod).digest()
+        return SP800_108_Counter(
+            master=key, key_len=k, prf=hmac_sha2, label=label, context=context
+        )
+
+    @classmethod
+    def derive(self, key, constant):
+        return self.random_to_key(
+            self.kdf_hmac_sha2(key=key.contents, label=constant, k=self.macsize)
+        )
+
+    @classmethod
+    def prf(self, input_key, string):
+        return self.kdf_hmac_sha2(
+            key=input_key.contents, label=b"prf", k=self.seedsize, context=string
+        )
+
+    @classmethod
+    def string_to_key(self, string, salt, params):
+        if not isinstance(string, bytes):
+            string = string.encode("utf-8")
+        if not isinstance(salt, bytes):
+            salt = salt.encode("utf-8")
+
+        saltp = self.enctype_name + b"\0" + salt
+
+        iter_count = struct.unpack(">L", params)[0] if params else 32768
+        tkey = PBKDF2(
+            password=string,
+            salt=saltp,
+            count=iter_count,
+            dkLen=self.keysize,
+            hmac_hash_module=self.hashmod,
+        )
+        return self.random_to_key(
+            self.kdf_hmac_sha2(key=tkey, label=b"kerberos", k=self.keysize)
+        )
+
+    @classmethod
+    def encrypt(self, key, keyusage, plaintext, confounder):
+        ke = self.random_to_key(
+            self.kdf_hmac_sha2(
+                key.contents, struct.pack(">IB", keyusage, 0xAA), self.keysize
+            )
+        )
+        ki = self.random_to_key(
+            self.kdf_hmac_sha2(
+                key.contents, struct.pack(">IB", keyusage, 0x55), self.macsize
+            )
+        )
+        n = _random_bytes(self.blocksize)
+        # Initial cipher state is a zeroed buffer
+        iv = bytes(self.blocksize)
+        c = self.basic_encrypt(ke, n + plaintext, iv)
+        h = HMAC.new(ki.contents, iv + c, self.hashmod).digest()
+        ciphertext = c + h[: self.macsize]
+        assert plaintext == self.decrypt(key, keyusage, ciphertext)
+        return ciphertext
+
+    @classmethod
+    def decrypt(self, key, keyusage, ciphertext):
+        if not isinstance(ciphertext, bytes):
+            ciphertext = bytes(ciphertext)
+        ke = self.random_to_key(
+            self.kdf_hmac_sha2(
+                key.contents, struct.pack(">IB", keyusage, 0xAA), self.keysize
+            )
+        )
+        ki = self.random_to_key(
+            self.kdf_hmac_sha2(
+                key.contents, struct.pack(">IB", keyusage, 0x55), self.macsize
+            )
+        )
+        c = ciphertext[: -self.macsize]
+        h = ciphertext[-self.macsize :]
+        # Initial cipher state is a zeroed buffer
+        iv = bytes(self.blocksize)
+        if h != HMAC.new(ki.contents, iv + c, self.hashmod).digest()[: self.macsize]:
+            raise InvalidChecksum("ciphertext integrity failure")
+        plaintext = self.basic_decrypt(ke, c, iv)[self.blocksize :]
+        return plaintext
+
+
+class _AES128_SHA1(_EtypeRfc3962):
+    enctype = EncryptionTypes.AES128_CTS_HMAC_SHA1_96
     keysize = 16
     seedsize = 16
     hashmod = SHA1
 
 
-class _AES256_SHA1(_AesXXXSha1):
-    etype = EncryptionTypes.AES128_CTS_HMAC_SHA256_128
+class _AES256_SHA1(_EtypeRfc3962):
+    enctype = EncryptionTypes.AES256_CTS_HMAC_SHA1_96
     keysize = 32
     seedsize = 32
     hashmod = SHA1
 
 
-class _AES128_SHA256:
-    etype = EncryptionTypes.AES128_CTS_HMAC_SHA256_128
+class _AES128_SHA256(_EtypeRfc8009):
+    enctype = EncryptionTypes.AES128_CTS_HMAC_SHA256_128
     seedsize = 256 // 8
     macsize = 128 // 8
     keysize = 128 // 8
     hashmod = SHA256
+    enctype_name = b"aes128-cts-hmac-sha256-128"
 
 
-class _AES256_SHA384:
-    etype = EncryptionTypes.AES256_CTS_HMAC_SHA384_192
+class _AES256_SHA384(_EtypeRfc8009):
+    enctype = EncryptionTypes.AES256_CTS_HMAC_SHA384_192
     seedsize = 384 // 8
     macsize = 192 // 8
     keysize = 256 // 8
     hashmod = SHA384
+    enctype_name = b"aes256-cts-hmac-sha384-192"
 
 
 def _get_etype_profile(etype: EncryptionTypes):
@@ -175,7 +284,7 @@ def _get_etype_profile(etype: EncryptionTypes):
 # dictionary to hold classes for enctypriont
 _etype_table: dict = {
     EncryptionTypes.AES128_CTS_HMAC_SHA1_96: _AES128_SHA1,
-    # EncryptionTypes.AES128_CTS_HMAC_SHA256_128: _AES128_SHA256,
-    # EncryptionTypes.AES256_CTS_HMAC_SHA1_96: _AES256_SHA1,
-    # EncryptionTypes.AES256_CTS_HMAC_SHA384_192: _AES256_SHA384,
+    EncryptionTypes.AES128_CTS_HMAC_SHA256_128: _AES128_SHA256,
+    EncryptionTypes.AES256_CTS_HMAC_SHA1_96: _AES256_SHA1,
+    EncryptionTypes.AES256_CTS_HMAC_SHA384_192: _AES256_SHA384,
 }
--- ipapocket/krb5/crypto/crypto.py
+++ ipapocket/krb5/crypto/crypto.py
@@ -14,12 +14,8 @@ def string_to_key(etype, data, salt, params=None):
 
 def supported_enctypes() -> list[EncryptionTypes]:
     enctypes = list()
-    # enctypes.append(EncryptionTypes.ARCFOUR_HMAC)
-    # enctypes.append(EncryptionTypes.DES3_CBC_SHA1)
     enctypes.append(EncryptionTypes.AES128_CTS_HMAC_SHA1_96)
     enctypes.append(EncryptionTypes.AES256_CTS_HMAC_SHA1_96)
     enctypes.append(EncryptionTypes.AES128_CTS_HMAC_SHA256_128)
     enctypes.append(EncryptionTypes.AES256_CTS_HMAC_SHA384_192)
-    # enctypes.append(EncryptionTypes.CAMELLIA128_CTS_CMAC)
-    # enctypes.append(EncryptionTypes.CAMELLIA256_CTS_CMAC)
     return enctypes

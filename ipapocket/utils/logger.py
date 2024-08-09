c2fe111eaf7020fb252475960b9ffbad1b5bf046
h3x0c4t <none>
2024-08-09 12:16:15 +0300

    add logger

---
+++ ipapocket/utils/logger.py
@@ -0,0 +1,37 @@
+import logging
+import sys
+
+class LoggerFormatter(logging.Formatter):
+    def __init__(self):
+        logging.Formatter.__init__(self,'%(bullet)s %(message)s', None)
+
+    def format(self, record):
+        match record.levelno:
+            case logging.INFO:
+                record.bullet = '[+]'
+            case logging.WARNING:
+                record.bullet = '[!]'
+            case logging.DEBUG:
+                record.bullet = '[*]'
+            case logging.ERROR:
+                record.bullet = '[-]'
+            case _:
+                record.bullet = '[x]'
+
+        return logging.Formatter.format(self, record)
+  
+class LoggerFormatterTimeStamp(LoggerFormatter):
+    def __init__(self):
+        logging.Formatter.__init__(self,'[%(asctime)-15s] %(bullet)s %(message)s', None)
+
+    def formatTime(self, record, datefmt=None):
+        return LoggerFormatter.formatTime(self, record, datefmt="%Y-%m-%d %H:%M:%S")
+
+def init(ts=False):
+    handler = logging.StreamHandler(sys.stdout)
+    if not ts:
+        handler.setFormatter(LoggerFormatter())
+    else:
+        handler.setFormatter(LoggerFormatterTimeStamp())
+    logging.getLogger().addHandler(handler)
+    logging.getLogger().setLevel(logging.INFO)

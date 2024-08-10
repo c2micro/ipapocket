34cec54819b0978774ceadaa968e8da268db4fce
38dh91 <>
2024-08-11 00:03:49 +0300

    update user_enum with new modeling

--- examples/user_enum.py
+++ examples/user_enum.py
@@ -7,9 +7,9 @@ sys.path.append(".")
 
 from ipapocket.network.krb5 import Krb5Client
 from ipapocket.krb5.operations import as_req_wihtout_pa
-from ipapocket.krb5.asn1 import *
 from ipapocket.krb5.constants import ErrorCodes
 from ipapocket.utils import logger
+from ipapocket.krb5.objects import KerberosResponse
 
 
 class UserEnum:
@@ -31,29 +31,22 @@ class UserEnum:
             as_req = as_req_wihtout_pa(self._domain, username)
             data = self._krb5_client.sendrcv(as_req.to_asn1().dump())
             # convert to response type
-            krb_msg = KerberosResponseAsn1.load(data)
-            if krb_msg.name != "KRB-ERROR":
-                # client doesn't need preauth, so we get TGT right now
-                # TODO
-                self.printer(username, "without preauth")
-            else:
-                # if we need password (and user is active)
-                if (
-                    krb_msg.native["error-code"]
-                    == ErrorCodes.KDC_ERR_PREAUTH_REQUIRED.value
-                ):
+            response = KerberosResponse.load(data)
+            if response.is_krb_error:
+                rep = response.krb_error
+                if rep.error_code == ErrorCodes.KDC_ERR_PREAUTH_REQUIRED:
                     self.printer(username, "need password")
-                # if user's password is disabled or account locked out in LDAP (krb will not help with determine)
-                if krb_msg.native["error-code"] == ErrorCodes.KDC_ERR_KEY_EXPIRED.value:
+                if rep.error_code == ErrorCodes.KDC_ERR_KEY_EXPIRED:
+                    # we can't determine this via kerberos
                     self.printer(
                         username, "password expired or client disabled in LDAP"
                     )
-                # if user is locked out, but password has not been expired
-                if (
-                    krb_msg.native["error-code"]
-                    == ErrorCodes.KDC_ERR_CLIENT_REVOKED.value
-                ):
+                if rep.error_code == ErrorCodes.KDC_ERR_CLIENT_REVOKED:
                     self.printer(username, "client locked out")
+            else:
+                # client doesn't need preauth
+                self.printer(username, "without preauth")
+                pass
 
 
 def main():

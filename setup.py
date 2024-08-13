f544f75da40f9b0969eb11a961fe5a034cc039eb
nu11zy <55335158+nu11zy@users.noreply.github.com>
2024-08-13 20:23:58 +0300

    init

---
+++ setup.py
@@ -0,0 +1,31 @@
+import glob
+import os
+from setuptools import setup
+
+VERSION = '0.1'
+
+def read(fname):
+    return open(fname).read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='ipapocket',
+    version = '0.1',
+    description="Toolset for interaction with FreeIPA",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url = "https://github.com/c2micro/ipapocket",
+    author = "c2micro",
+    maintainer="c2micro",
+    platforms=["Unix"],
+    packages=['ipapocket', 'ipapocket.exceptions', 'ipapocket.krb5', 'ipapocket.krb5.crypto', 'ipapocket.network', 'ipapocket.utils'],
+    scripts=glob.glob(os.path.join('examples', '*.py')),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
